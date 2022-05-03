2afaf820317fb340610891917af8e0a0b3e5f552
Chance Gardner <Chantz465@gmail.com>
2022-05-03 11:11:32 -0500

    (first add)

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simlating a deposit of {amount,0:c} to your account");
+            _balance = amount;
+        
+        
+        }
+
+        public double GetBalance()
+            { return _balance; }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("how much money do you want to deposit into your account?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+            double userBalance =  account.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
+        }
+    }
+
+
+
+
+
+
+}
